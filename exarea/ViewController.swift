d8a67d6ea79e2c0064e2532ec0c071d1f9e908b3
SoRush <s.javanmard@gandom.co>
2018-09-26 05:06:29 +0330

    Initial Commit

---
+++ exarea/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exarea
+//
+//  Created by Soroush on 7/4/1397 AP.
+//  Copyright © 1397 tamtom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
