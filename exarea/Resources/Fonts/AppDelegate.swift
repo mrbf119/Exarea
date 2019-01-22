6e3f8fea670ef549a82630b2e4027eb4da43893b
SoRush <s.javanmard@gandom.co>
2019-01-22 14:46:17 +0330

    add barcode

--- exarea/Resources/Fonts/AppDelegate.swift
+++ exarea/Resources/Fonts/AppDelegate.swift
@@ -12,7 +12,7 @@ import SideMenu
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    var window: UIWindow? 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UIApplication.shared.statusBarStyle = .lightContent
