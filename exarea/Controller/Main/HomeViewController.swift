e42dac189eb8669e264605363895b9c3ab216403
SoRush <s.javanmard@gandom.co>
2019-02-09 21:13:20 +0330

    set default tab to home

--- exarea/Controller/Main/HomeViewController.swift
+++ exarea/Controller/Main/HomeViewController.swift
@@ -32,7 +32,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.getData()
+        self.getData()
     }
     
     private func getData() {
